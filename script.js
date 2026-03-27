cc8ff221ec8f1c2abf30b3d0f0c492ba1a22af89
harsh-frontdev <harshp.0009@gmail.com>
2026-03-27 11:55:24 +0530

    feat: Implement project creation, search filtering, and data synchronization with local storage and dynamic table updates.

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // Get Project List from Local Storage
-const projectsList = localStorage.getItem('projectsList');
-if(projectsList === null){
-    console.log('Project List Not Found');
-    localStorage.setItem('projectsList', '');
-}
+let projectsList = JSON.parse(localStorage.getItem('projectsList') || '[]');
 
 // All the Elements
 const clientNameEl = document.querySelector('#client-name');
@@ -14,6 +10,7 @@ const searchBarEl = document.querySelector('#search-bar');
 const syncBtnEl = document.querySelector('#sync-btn');
 const projectTableEl = document.querySelector('#project-table');
 const projectBodyEl = document.querySelector('#project-body');
+const loaderEl = document.querySelector('#loader');
 
 // Project Class
 class Project {
@@ -27,14 +24,17 @@ class Project {
         return `$${this.budget}`;
     }
 }
-const newProject = new Project('Car', 'Script', 100);
 
 // Render Table with Data
 function renderTable(data){
-    if(data){
-        console.log(data);
-    }else {
-        projectBodyEl.innerHTML += `<tr><td colspan="5" style="text-align: center;">No Client Found</td></tr>`;
+    projectBodyEl.innerHTML = '';
+    if(data.length){
+        data.forEach(element => {
+            const project = new Project(element.name, element.platform, element.budget);
+            projectBodyEl.innerHTML += `<tr><td>${project.name}</td><td>${project.platform}</td><td>${project.formatBudget()}</td><td>Active</td><td>Delete</td></tr>`
+        });
+    } else {
+        projectBodyEl.innerHTML = `<tr><td colspan="5" style="text-align: center;">No Client Found</td></tr>`;
     }
 }
 renderTable(projectsList);
@@ -42,5 +42,42 @@ renderTable(projectsList);
 // Add Project on Click
 clientForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('Got Data');
+    const newProject  = new Object({id: Date.now(), name: clientNameEl.value, platform: platformEl.value, budget: budgetEl.value, status: 'active'});
+    projectsList.push(newProject);
+    localStorage.setItem('projectsList', JSON.stringify(projectsList));
+    renderTable(projectsList);
+    clientNameEl.value = '';
+    budgetEl.value = '';
 })
+
+// Filter Projects
+searchBarEl.addEventListener('input', (e) => {
+    const searchValue = e.target.value.toLowerCase().trim();
+
+    if(searchValue.length > 0){
+        projectBodyEl.innerHTML = '';
+        projectsList = JSON.parse(localStorage.getItem('projectsList') || '[]');
+        
+        const filteredProjects = projectsList.filter((element) => element.name.toLowerCase().includes(searchValue));
+        renderTable(filteredProjects);
+    } else {
+        renderTable(projectsList);
+    }
+})
+
+// Sync Data
+async function syncData(duration){
+    return new Promise((resolve, reject) => {
+        projectBodyEl.innerHTML = '';
+        loaderEl.classList.remove('hidden')
+        setTimeout(() => {
+            loaderEl.classList.add('hidden')
+            renderTable(projectsList);
+            resolve('Data Synced');
+        } ,duration);
+    });
+}
+
+syncBtnEl.addEventListener('click', () => {
+    syncData(2000)
+});
